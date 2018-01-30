b652885bffbe5ab97a3e2943bf363a6e2a64df92
Unknown <cesar.moreno@alkanza.us>
2018-01-30 14:52:08 -0500

    [Update] Transform divs into tables for gmail compatibility

--- app/index.js
+++ app/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 
-import './styles/index.scss'
 import App from './App'
 import createStore from './store'
 
